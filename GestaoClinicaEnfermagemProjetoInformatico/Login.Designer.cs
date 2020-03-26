f836bcffcd456f6e2e39b68f356dde3b0c02894d
2160844 <2160844@my.ipleiria.pt>
2020-03-26 22:43:31 +0000

    Quase Login, ja vai a BD

--- GestaoClinicaEnfermagemProjetoInformatico/Login.Designer.cs
+++ GestaoClinicaEnfermagemProjetoInformatico/Login.Designer.cs
@@ -201,7 +201,7 @@
             this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCancelar.ForeColor = System.Drawing.Color.Black;
             this.btnCancelar.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.btnCancelar.Location = new System.Drawing.Point(967, 329);
+            this.btnCancelar.Location = new System.Drawing.Point(879, 313);
             this.btnCancelar.Margin = new System.Windows.Forms.Padding(2);
             this.btnCancelar.Name = "btnCancelar";
             this.btnCancelar.RightToLeft = System.Windows.Forms.RightToLeft.No;
@@ -220,6 +220,7 @@
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(492, 29);
             this.txtPassword.TabIndex = 13;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
             // 
             // btnLogin
             // 
@@ -230,16 +231,17 @@
             this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnLogin.ForeColor = System.Drawing.Color.Black;
             this.btnLogin.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
-            this.btnLogin.Location = new System.Drawing.Point(967, 199);
+            this.btnLogin.Location = new System.Drawing.Point(879, 183);
             this.btnLogin.Margin = new System.Windows.Forms.Padding(2);
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.btnLogin.Size = new System.Drawing.Size(170, 78);
             this.btnLogin.TabIndex = 1;
-            this.btnLogin.Text = "Guardar";
+            this.btnLogin.Text = "Entrar";
             this.btnLogin.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             this.btnLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
             // 
             // lblPassword
             // 
@@ -259,6 +261,7 @@
             this.txtUsername.Name = "txtUsername";
             this.txtUsername.Size = new System.Drawing.Size(492, 29);
             this.txtUsername.TabIndex = 11;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
             // 
             // lblUsername
             // 
--- GestaoClinicaEnfermagemProjetoInformatico/Login.cs
+++ GestaoClinicaEnfermagemProjetoInformatico/Login.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Globalization;
 
 namespace GestaoClinicaEnfermagemProjetoInformatico
 {
     public partial class Login : Form
     {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand com = new SqlCommand();
         public Login()
         {
             InitializeComponent();
+            conn.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SiltesSaude;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         }
 
         private void Login_Load(object sender, EventArgs e)
@@ -30,6 +36,41 @@ namespace GestaoClinicaEnfermagemProjetoInformatico
         private void panelFormulario_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            if (txtUsername.Text.Equals(@"Username"))
+            {
+                txtUsername.Text = "";
+            }
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            if (txtPassword.Text.Equals(@"Password"))
+            {
+                txtPassword.Text = "";
+            }
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            com.Connection = conn;
+            com.CommandText = "select username, password from Enfermeiro";
+            SqlDataReader dataReader = com.ExecuteReader();
+            if (dataReader.Read()) {
+                if (txtUsername.Text.Equals(dataReader["username"].ToString()) && txtPassword.Text.Equals(dataReader["password"].ToString()))
+                {
+                    MessageBox.Show("Login Efetuado com Sucesso", "Parabéns", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Nome de utilizador ou palavra passe errados. Volte a tentar.", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            conn.Close();
         }
     }
 }
